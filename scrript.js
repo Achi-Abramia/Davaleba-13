8dc9f19f1a4a4cd0122d6abe196e1fb30307aede
Achi-Abramia <163457483+Achi-Abramia@users.noreply.github.com>
2024-04-23 22:37:11 +0400

    Create scrript.js

---
+++ scrript.js
@@ -0,0 +1,50 @@
+// Davasleba-01
+
+// შევქმნათ ყველა მარტივი ტიპის ცვლადი რაც ვისწავლეთ
+
+var location = "Tbilisi";
+let amount = 40000;
+const isValuable = true;
+
+// Davaleba-02
+
+// არითმეტიკული ოპერაციები მიმატება, გამოკლება, გაყოფა, გამრავლება ყველაფერი რაც ვისწავლეთ
+
+let n = 14;
+let m = 8;
+let sum = n + m;
+console.log(sum);
+
+let a = 94;
+let b = 74;
+let difference = a - b;
+console.log(difference);
+
+let c = 4200;
+let d = 60;
+let division = c / d;
+console.log(division);
+
+let j = 53;
+let k = 67;
+let multiply = j * k;
+console.log(multiply);
+
+// davalevba-03 
+
+// სტრინგების კონკატენაცია + ით და სტრინგების კონკატენაცია `ბექტიკებით`
+
+let supply = 35000;
+let person = 340;
+console.log("Hello, we sent medical supply, " + supply + " box. Order is to divide it to " + person + " wounded soldier.");
+console.log(`Hello, we sent medical supply, ${supply} box. Order is to divide it to ${person} wounded soldier. `);
+
+// davaleba-04
+
+// მოცემულა მართკუთხედი სიგრძე ცვლადი = 7 და სიგანე = 5 (შექმენით 2 ცვლადი) გამოთვალეთ მართკუთხედის ფართობი ამ 2 ცვლადის გამოყენებით
+
+let length = 7;
+let width = 5;
+let area = length * width;
+console.log(area);
+
